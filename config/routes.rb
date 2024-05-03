2687996148551ca2cc3abdfd3110ce5c52e2c887
oott0228 <oott0228@gmail.com>
2024-05-03 09:08:57 +0900

    登録されたイベントへの参加機能、参加キャンセル機能を作る

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :events
   root "welcome#index"
   get "/auth/:provider/callback" => "sessions#create"
-  delete "/logout" => "sessions#destroy"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  delete "/logout" => "sessions#destroy", as: :logout
+
+  resources :events do
+    resources :tickets
+  end
 end
