45ccb8ebc0ad75866cae8b2394bc2520c997e49c
oott0228 <oott0228@gmail.com>
2024-05-09 08:55:16 +0900

    モデルに対する機能テスト

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 require_relative 'sign_in_helper'
+require 'minitest/mock'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
